5f582cd9f74d5a482f19ede965c2c4492bac4354
罗伟 <weiluo@stu.xjtu.edu.cn>
2024-01-03 11:40:11 +0800

    后端

---
+++ tags_web_back/tags_web_back/src/test/java/com/example/tags_web_back/TagsWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tags_web_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagsWebBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
