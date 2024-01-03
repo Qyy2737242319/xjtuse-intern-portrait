a6940b762d0205ba380f4d431451b7a80d25cda5
罗伟 <weiluo@stu.xjtu.edu.cn>
2024-01-03 11:40:11 +0800

    后端最终springboot

---
+++ tags_web_back/tags_web_back/src/main/java/com/example/tags_web_back/TagsWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tags_web_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagsWebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagsWebBackApplication.class, args);
+    }
+
+}
