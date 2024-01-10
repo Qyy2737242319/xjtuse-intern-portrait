8a5c889f5cf40074a77467edee33d5923dd8b21c
Strive236 <1595709321@qq.com>
2024-01-10 15:32:42 +0800

    mapper改interface

--- tags_web_back/tags_web_back/src/main/java/com/example/tags_web_back/dao/TagsMapper.java
+++ tags_web_back/tags_web_back/src/main/java/com/example/tags_web_back/dao/TagsMapper.java
@@ -3,6 +3,6 @@ package com.example.tags_web_back.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class TagsMapper {
+public interface TagsMapper {
 
 }
--- tags_web_back/tags_web_back/src/main/java/com/example/tags_web_back/dao/UserTagsMapper.java
+++ tags_web_back/tags_web_back/src/main/java/com/example/tags_web_back/dao/UserTagsMapper.java
@@ -3,5 +3,5 @@ package com.example.tags_web_back.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class UserTagsMapper {
+public interface UserTagsMapper {
 }
