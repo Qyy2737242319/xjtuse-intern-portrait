7231b5e758e2866ab7871302cd66dedf929f3156
Strive236 <1595709321@qq.com>
2024-01-10 15:23:12 +0800

    config层创建

---
+++ tags_web_back/tags_web_back/src/main/java/com/example/tags_web_back/config/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.example.tags_web_back.config;
+
+import lombok.Data;
+
+import java.util.HashMap;
+
+@Data
+public final class ApiResponse {
+
+  private Integer code;
+  private String errorMsg;
+  private Object data;
+
+  private ApiResponse(int code, String errorMsg, Object data) {
+    this.code = code;
+    this.errorMsg = errorMsg;
+    this.data = data;
+  }
+  
+  public static ApiResponse ok() {
+    return new ApiResponse(0, "", new HashMap<>());
+  }
+
+  public static ApiResponse ok(Object data) {
+    return new ApiResponse(0, "", data);
+  }
+
+
+  public static ApiResponse error(String errorMsg) {
+    return new ApiResponse(1, errorMsg, new HashMap<>());
+  }
+}
